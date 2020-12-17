59abe7574ebed6da2cb2cec8dae59b339c71260c
carmeloleonardi <carmeloleonardi@outlook.com>
2020-12-17 18:37:41 +0100

    first commit

---
+++ PrimeNumberWebApp.Identity/Data/Migrations/20201217160854_DatabasePrimeNumber.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrimeNumberWebApp.Identity.Data.Migrations
+{
+    public partial class DatabasePrimeNumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
